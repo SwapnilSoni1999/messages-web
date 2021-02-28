0efa5b12405d078ad6d028643bf5605801df9383
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-02-28 05:52:48 +0530

    add messageService that fetches inbox

--- README.md
+++ README.md
@@ -15,6 +15,8 @@ const MessagesClient = require('./messages-web')
 const client = new MessagesClient()
 
 client.on('qr-code', (base64Image) => {
+    // example code to save image
+    fs.writeFileSync('qr.jpg', base64Image.replace(/^data:image\/png;base64,/, ""), { encoding: 'base64' })
     // your code
 })
 ```
--- lib/client.ts
+++ lib/client.ts
@@ -3,27 +3,28 @@ import puppeteer from 'puppeteer'
 
 type ClientEvents = 'browser-launched' | 'qr-code' | 'authenticated'
 
-declare interface MessagesClient {
-    on(event: ClientEvents, listener: (data: any) => void): this
-    emit(event: ClientEvents, emitter?: (data: any) => void): boolean
+interface MyClassEvents {
+    'authenticated': (service: MessageService) => void,
+    'browser-launched': () => void,
+    'qr-code': (base64Image: string) => void
 }
+declare interface MessagesClient {
+    on<U extends keyof MyClassEvents>(
+      event: U, listener: MyClassEvents[U]
+    ): this;
+  
+    emit<U extends keyof MyClassEvents>(
+      event: U, ...args: Parameters<MyClassEvents[U]>
+    ): boolean;
+  }
 
 class MessagesClient extends EventEmitter implements MessagesClient {
     page!: puppeteer.Page
     browser!: puppeteer.Browser
 
-    private qrObserver: MutationObserver | undefined
-
     constructor (headless=true) {
         super()
         this.launch(headless)
-        this.page?.on('request', request => {
-            const url = request.url()
-            if (url.includes('web/conversations')) {
-                this.emit('authenticated') // todo: pass credentials as well
-                this.qrObserver?.disconnect()
-            }
-        })
     }
 
     private async launch (headless=true) {
@@ -38,7 +39,13 @@ class MessagesClient extends EventEmitter implements MessagesClient {
         })
         this.emit('browser-launched')
         this.attachQrReader()
-        
+        this.page.on('request', request => {
+            const url = request.url()
+            if (url.includes('Pairing/GetWebEncryptionKey')) {
+                const service = new MessageService(this.page)
+                this.emit('authenticated', service) // todo: pass credentials as well
+            }
+        })
         return
     }
 
@@ -54,7 +61,7 @@ class MessagesClient extends EventEmitter implements MessagesClient {
             }
         })
 
-        this.qrObserver = await this.page.evaluate(() => {
+        await this.page.evaluate(() => {
             const observer = new MutationObserver((mutations) => {
                 for (const mutation of mutations) {
                     if (mutation.attributeName === 'data-qr-code') {
@@ -93,6 +100,66 @@ class MessagesClient extends EventEmitter implements MessagesClient {
 
     async quit() {
         await this.browser.close()
+    }
+}
+
+type Conversation = {
+    unread: boolean, 
+    id: number,
+    timestamp: string, 
+    from: string, 
+    latestMsgText: string
+}
+class MessageService {
+    private page: puppeteer.Page
+    constructor (page: puppeteer.Page) {
+        this.page = page
+    }
+
+    async getInbox() {
+        
+        await this.page.waitForNavigation({ waitUntil: 'load' })
+        await this.page.waitForSelector('body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-main-nav > mws-conversations-list > nav > div.conv-container.ng-star-inserted > mws-conversation-list-item')
+
+        const inbox = await this.page.evaluate(() => {
+            function evalConvoElement (conversation: Element) {
+                const props:Conversation = {
+                    unread: false, // querySelector find .unread class
+                    id: 0, // href of a tag
+                    timestamp: '', // mws-relative-timestamp .innerText || > ..ng-star-inserted').getAttribute('aria-label') if latest message
+                    from: '', // querySelector('h3').innerText
+                    latestMsgText: '' // querySelector('mws-conversation-snippet').innerText
+                }
+                props.unread = conversation.querySelector('.unread') ? true : false
+                
+                const regex = /conversations\/(\d{1,})/g
+                const chatUrl = conversation.querySelector('a').href
+                props.id = parseInt(chatUrl.match(regex)[0].split('conversations/')[1])
+                
+                if (conversation.querySelector('mws-relative-timestamp').childElementCount > 0) {
+                    props.timestamp = conversation.querySelector('mws-relative-timestamp > .ng-star-inserted').getAttribute('aria-label')
+                } else {
+                    props.timestamp = (conversation.querySelector('mws-relative-timestamp') as HTMLElement).innerText
+                }
+
+                props.from = conversation.querySelector('h3').innerText
+                props.latestMsgText = (conversation.querySelector('mws-conversation-snippet') as HTMLElement).innerText
+                if (props.latestMsgText.startsWith('You:')) {
+                    props.latestMsgText = props.latestMsgText.slice('You:'.length).trim()
+                }
+                return props
+            }
+
+            const conversations = document.querySelectorAll("body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-main-nav > mws-conversations-list > nav > div.conv-container.ng-star-inserted > mws-conversation-list-item")
+            const msgs = []
+            for (const conversation of conversations) {
+                if (conversation) {
+                    msgs.push(evalConvoElement(conversation))
+                }
+            }
+            return msgs
+        })
+        return inbox
     }
 }
 
