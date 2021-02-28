338be90397a16b50c24e70ffd6432f2698c9a441
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-02-28 05:58:43 +0530

    add new docs for getInbox

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ git clone https://github.com/SwapnilSoni1999/messages-web.git
 2. Use it
 ```js
 const MessagesClient = require('./messages-web')
+const fs = require('fs')
 
 const client = new MessagesClient()
 
@@ -18,5 +19,10 @@ client.on('qr-code', (base64Image) => {
     // example code to save image
     fs.writeFileSync('qr.jpg', base64Image.replace(/^data:image\/png;base64,/, ""), { encoding: 'base64' })
     // your code
+})
+
+client.on('authenticated', async (service) => {
+    const inbox = service.getInbox()
+    await client.quit()
 })
 ```
--- lib/client.ts
+++ lib/client.ts
@@ -1,6 +1,8 @@
 import { EventEmitter } from 'events'
 import puppeteer from 'puppeteer'
 
+import MessageService from './service'
+
 type ClientEvents = 'browser-launched' | 'qr-code' | 'authenticated'
 
 interface MyClassEvents {
@@ -16,6 +18,10 @@ declare interface MessagesClient {
     emit<U extends keyof MyClassEvents>(
       event: U, ...args: Parameters<MyClassEvents[U]>
     ): boolean;
+
+    // removeListener<U extends keyof MyClassEvents>(
+    //     event: U
+    // )
   }
 
 class MessagesClient extends EventEmitter implements MessagesClient {
@@ -100,66 +106,6 @@ class MessagesClient extends EventEmitter implements MessagesClient {
 
     async quit() {
         await this.browser.close()
-    }
-}
-
-type Conversation = {
-    unread: boolean, 
-    id: number,
-    timestamp: string, 
-    from: string, 
-    latestMsgText: string
-}
-class MessageService {
-    private page: puppeteer.Page
-    constructor (page: puppeteer.Page) {
-        this.page = page
-    }
-
-    async getInbox() {
-        
-        await this.page.waitForNavigation({ waitUntil: 'load' })
-        await this.page.waitForSelector('body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-main-nav > mws-conversations-list > nav > div.conv-container.ng-star-inserted > mws-conversation-list-item')
-
-        const inbox = await this.page.evaluate(() => {
-            function evalConvoElement (conversation: Element) {
-                const props:Conversation = {
-                    unread: false, // querySelector find .unread class
-                    id: 0, // href of a tag
-                    timestamp: '', // mws-relative-timestamp .innerText || > ..ng-star-inserted').getAttribute('aria-label') if latest message
-                    from: '', // querySelector('h3').innerText
-                    latestMsgText: '' // querySelector('mws-conversation-snippet').innerText
-                }
-                props.unread = conversation.querySelector('.unread') ? true : false
-                
-                const regex = /conversations\/(\d{1,})/g
-                const chatUrl = conversation.querySelector('a').href
-                props.id = parseInt(chatUrl.match(regex)[0].split('conversations/')[1])
-                
-                if (conversation.querySelector('mws-relative-timestamp').childElementCount > 0) {
-                    props.timestamp = conversation.querySelector('mws-relative-timestamp > .ng-star-inserted').getAttribute('aria-label')
-                } else {
-                    props.timestamp = (conversation.querySelector('mws-relative-timestamp') as HTMLElement).innerText
-                }
-
-                props.from = conversation.querySelector('h3').innerText
-                props.latestMsgText = (conversation.querySelector('mws-conversation-snippet') as HTMLElement).innerText
-                if (props.latestMsgText.startsWith('You:')) {
-                    props.latestMsgText = props.latestMsgText.slice('You:'.length).trim()
-                }
-                return props
-            }
-
-            const conversations = document.querySelectorAll("body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-main-nav > mws-conversations-list > nav > div.conv-container.ng-star-inserted > mws-conversation-list-item")
-            const msgs = []
-            for (const conversation of conversations) {
-                if (conversation) {
-                    msgs.push(evalConvoElement(conversation))
-                }
-            }
-            return msgs
-        })
-        return inbox
     }
 }
 
