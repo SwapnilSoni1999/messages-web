1f8e4d68e32bfb083dae91b1b9d626788a170445
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-03-02 02:46:52 +0530

    servie: introduce sendMessage function

--- lib/service.ts
+++ lib/service.ts
@@ -14,7 +14,7 @@ class MessageService {
     }
 
     async getInbox() {
-        
+        // TODO: add pagination
         await this.page.waitForNavigation({ waitUntil: 'load' })
         await this.page.waitForSelector('body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-main-nav > mws-conversations-list > nav > div.conv-container.ng-star-inserted > mws-conversation-list-item')
 
@@ -57,6 +57,49 @@ class MessageService {
             return msgs
         })
         return inbox
+    }
+
+    async sendMessage (to: string, text: string) {
+        await this.page.waitForNavigation({ waitUntil: 'load' })
+        await this.page.waitForSelector('body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-main-nav > mws-conversations-list > nav > div.conv-container.ng-star-inserted > mws-conversation-list-item')
+
+        // TODO: parse to var to check if country code is included or not
+        const newChatBtn = await this.page.$('body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-main-nav > div > mw-fab-link > a')
+        await newChatBtn.click()
+        await this.page.waitForNavigation({ waitUntil: 'domcontentloaded' })
+        // await page.waitForSelector()
+        // const numberInput = await page.$eval('#mat-chip-list-2 > div > input', (input) => {
+        //     console.log(input)
+        // })
+        // const numberInput = await page.$('#mat-chip-list-2 > div > input')
+        try {
+            await this.page.waitForXPath('//*[@id="mat-chip-list-0"]/div/input')
+        } catch (err) { }
+        // await page.waitForTimeout(2 * 1000) // remove lateer
+        let numberInput = await this.page.$x('//*[@id="mat-chip-list-0"]/div/input')
+        // console.log('NumberInput', numberInput)
+        if (numberInput.length) {
+            await numberInput[0].type(to)
+            // numberInput.type(String.fromCharCode(13))
+            await this.page.waitForXPath('/html/body/mw-app/mw-bootstrap/div/main/mw-main-container/div/mw-new-conversation-container/div/mw-contact-selector-button/button')
+            const contactBtn = await this.page.$x('/html/body/mw-app/mw-bootstrap/div/main/mw-main-container/div/mw-new-conversation-container/div/mw-contact-selector-button/button')
+            await contactBtn[0].click()
+        }
+        // await page.waitForSelector('body > mw-app > mw-bootstrap > div > main > mw-main-container > div > mw-conversation-container > div.container.ng-tns-c39-541.ng-star-inserted > div > mws-message-compose > div > div.input-box > div > mws-autosize-textarea > textarea', { visible: true })
+        try {
+            await this.page.waitForXPath('/html/body/mw-app/mw-bootstrap/div/main/mw-main-container/div/mw-conversation-container/div[1]/div/mws-message-compose/div/div[2]/div/mws-autosize-textarea/textarea')
+        } catch (err) {  }
+        // await page.waitForTimeout(2 * 1000) // remove lateer
+        let msgInput = await this.page.$x('/html/body/mw-app/mw-bootstrap/div/main/mw-main-container/div/mw-conversation-container/div[1]/div/mws-message-compose/div/div[2]/div/mws-autosize-textarea/textarea')
+        // console.log('MsgINput', msgInput)
+        if (msgInput) {
+            await msgInput[0].type(text)
+            await this.page.waitForXPath('/html/body/mw-app/mw-bootstrap/div/main/mw-main-container/div/mw-conversation-container/div[1]/div/mws-message-compose/div/div[2]/div/mws-message-send-button/button')
+            let sendBtn = await this.page.$x('/html/body/mw-app/mw-bootstrap/div/main/mw-main-container/div/mw-conversation-container/div[1]/div/mws-message-compose/div/div[2]/div/mws-message-send-button/button')
+            await sendBtn[0].click()
+        }
+        // TODO: return messageId
+        return 
     }
 }
 
