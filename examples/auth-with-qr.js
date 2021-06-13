5554187ff600a17ac106500948a644aa525585fa
Swapnil Soni <swapnil.soni12345@gmail.com>
2021-06-13 13:20:28 +0530

    example: fix qr code

--- examples/auth-with-qr.js
+++ examples/auth-with-qr.js
@@ -2,7 +2,7 @@ const { default: MessagesClient } = require('../lib/client')
 
 const client = new MessagesClient()
 
-client.on('qr-code', () => {
+client.on('qr-code', (base64Image) => {
     console.log('Saving QR to your local machine...')
     fs.writeFileSync('qr.jpg', base64Image.replace(/^data:image\/png;base64,/, ""), { encoding: 'base64' })
     console.log('QR code saved! Please open it and scan from your Google Messages App!')
